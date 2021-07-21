a53d31b2275dd725d096a37e6dd96938c63c301a
Juanu <juanu@juanu.com.ar>
2021-07-21 18:26:17 -0300

    Removed ignored build files

--- posta-lib/build/services/DummyPOHService.d.ts
+++
@@ -1,5 +0,0 @@
-import { IContractProvider } from "./ContractProvider";
-declare const _default: {
-    registerHuman(address: string, contractProvider: IContractProvider): Promise<any>;
-};
-export default _default;
--- posta-lib/build/services/configService.d.ts
+++
@@ -1,5 +0,0 @@
-import { ethers } from "ethers";
-declare const _default: {
-    getEthersProvider(): ethers.providers.BaseProvider | ethers.providers.JsonRpcProvider;
-};
-export default _default;
